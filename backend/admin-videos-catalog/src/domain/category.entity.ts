150aa577267f0da037fcece5572b4e0d984f6590
Wellington <wellington.map@hotmail.com>
2025-02-25 15:43:49 -0300

    feat(admin-videos-catalog): add category entity commands
    Implemented methods to update name, update description, activate, deactivate, and convert the category entity to JSON

--- backend/admin-videos-catalog/src/domain/category.entity.ts
+++ backend/admin-videos-catalog/src/domain/category.entity.ts
@@ -30,4 +30,30 @@ export class Category {
   static create(props: CategoryCreateCommand): Category {
     return new Category(props);
   }
+
+  changeName(name: string): void {
+    this.name = name;
+  }
+
+  changeDescription(description: string): void {
+    this.description = description;
+  }
+
+  activate(): void { 
+    this.is_active = true;
+  }
+
+  deactivate(): void {
+    this.is_active = false;
+  }
+  
+  toJSON(): CategoryProps {
+    return {
+      category_id: this.category_id,
+      name: this.name,
+      description: this.description,
+      is_active: this.is_active,
+      created_at: this.created_at
+    }
+  }  
 }
